31734686dd7886655410254b4a6d63575914a6b3
Hye Soo Park <flyjwayur@gmail.com>
2018-11-20 11:52:36 +0200

    Inital commit with create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      "Hei"
+      </div>
+    );
+  }
+}
+
+export default App;
