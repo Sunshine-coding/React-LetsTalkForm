d5fa76e2820eda02859224e3d5553c40ccb0367b
Hye Soo Park <flyjwayur@gmail.com>
2018-11-23 11:54:53 +0200

    Updates

---
+++ precache-manifest.7bebecf14413954b154b41e33f1faed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfefb9c29c592f7b3d89",
+    "url": "/React-LetsTalkForm/static/js/runtime~main.bfefb9c2.js"
+  },
+  {
+    "revision": "af4fcba8dc09e7e328ce",
+    "url": "/React-LetsTalkForm/static/js/main.af4fcba8.chunk.js"
+  },
+  {
+    "revision": "bcd660d22b8d57d77fa2",
+    "url": "/React-LetsTalkForm/static/js/1.bcd660d2.chunk.js"
+  },
+  {
+    "revision": "af4fcba8dc09e7e328ce",
+    "url": "/React-LetsTalkForm/static/css/main.a8b8a1ab.chunk.css"
+  },
+  {
+    "revision": "c70f6fe54a1a16c48dab830097b1be8a",
+    "url": "/React-LetsTalkForm/index.html"
+  }
+];
